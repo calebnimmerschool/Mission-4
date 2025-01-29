8c5a8ecd0cc36c827360335c7940080466a9ec9c
Miala44 <adamsmia7@gmail.com>
2025-01-29 13:29:27 -0700

    updating

--- board.cs
+++ board.cs
@@ -2,5 +2,31 @@ namespace Mission_4;
 
 public class board
 {
-    
+    int userInput;
+    string usedPositions = "";
+
+    private int[,] ticTacBoard;
+
+    public board(int[,] boardArray)
+    {
+        ticTacBoard = boardArray;
+    }
+
+    public void printBoard()
+    {
+        for (int i = 0; i < ticTacBoard.GetLength(0); i++)
+        {
+            for (int j = 0; j < ticTacBoard.GetLength(1); j++)
+            {
+                Console.Write(ticTacBoard[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    public void detectWinner()
+    {
+
+    }
 }
