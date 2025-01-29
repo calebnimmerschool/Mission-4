96762328eed5143eea469384e9c21b3598fb0e8d
Miala44 <adamsmia7@gmail.com>
2025-01-29 14:42:01 -0700

    added comments

--- board.cs
+++ board.cs
@@ -3,22 +3,27 @@ public class Board
     private char[,] ticTacBoard;
     private string[] positionLabels = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+    //constructor 
     public Board(char[,] boardArray)
     {
         ticTacBoard = boardArray;
     }
 
+    //method to print out board
     public void printBoard()
     {
-        int posIndex = 0;
-        for (int i = 0; i < ticTacBoard.GetLength(0); i++)
+        int posIndex = 0; 
+        for (int i = 0; i < ticTacBoard.GetLength(0); i++) //to get the row
         {
-            for (int j = 0; j < ticTacBoard.GetLength(1); j++)
+            for (int j = 0; j < ticTacBoard.GetLength(1); j++) //to get the column
             {
                 // Print position number if the cell is empty, otherwise print the cell value
+                //psuedo code for this line: if empty at this position of the array,
+                //print the number, otherwise print what the value is printed
                 Console.Write(ticTacBoard[i, j] == '\0' ? positionLabels[posIndex] : ticTacBoard[i, j].ToString());
+                
                 if (j < 2) Console.Write(" | ");
-                posIndex++;
+                posIndex++; //get next number position
             }
             Console.WriteLine();
             if (i < 2) Console.WriteLine("---------");
