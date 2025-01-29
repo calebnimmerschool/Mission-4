2d49626ed20c6f07c6c1e6c16581f0acb2bdf4ce
sarah <sdurham1@byu.edu>
2025-01-29 14:46:36 -0700

    Final

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// The “Driver” class (the Program.cs class with the main method where the program begins)
-// will:
-
+﻿
 //     • Create a game board array to store the players’ choices
 //     • Ask each player in turn for their choice and update the game board array
 //     • Print the board by calling the method in the supporting class
