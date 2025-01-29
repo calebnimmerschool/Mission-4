22c417aee2c00f35accfe5336042d8ad70c148b4
sarah <sdurham1@byu.edu>
2025-01-29 13:37:31 -0700

    We started the game in program.cs

--- Program.cs
+++ Program.cs
@@ -7,8 +7,37 @@
 //     • Check for a winner by calling the method in the supporting class, and notify the players
 // when a win has occurred and which player won the game
 
-// Welcome the user to the game
-Console.WriteLine("Welcome to our Tic Tac Toe Game!");
-Console.WriteLine("Please select a position 1-9 to place your 'x'.");
+// board b = new board();
+// string position = "";
+using System;
 
-board b = new board();
+namespace Mission4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to our Tic Tac Toe Game!");
+            char[,] gameBoard = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+            };
+
+            // Print the game board
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(gameBoard[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Welcome message
+            
+            Console.Write("Please select a position 1-9 to place your 'X': ");
+            Console.ReadLine();
+        }
+    }
+}
